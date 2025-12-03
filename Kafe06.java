c98673c22956113b45e2f339d7dbb2aaecd23000
ArifatulAfidah <faa9552@gmail.com>
2025-12-03 10:00:20 +0700

    percobaan 2

--- Kafe06.java
+++ Kafe06.java
@@ -1,5 +1,10 @@
 public class Kafe06 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, daptkan 10% untuk setiap pembelian!");
+        }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -11,6 +16,6 @@ public class Kafe06 {
         System.out.println("Silahkan Pilih Menu yang Anda inginkan");
     }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi" , true);
     }
 }
